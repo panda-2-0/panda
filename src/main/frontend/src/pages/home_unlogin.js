1a3a8833a6709d4f49309e012a0a0bf173d44e45
0exia099 <0exia099@gmail.com>
2023-06-05 17:36:43 +0900

    ㅇ

--- src/main/frontend/src/pages/home_unlogin.js
+++ src/main/frontend/src/pages/home_unlogin.js
@@ -81,7 +81,7 @@ function HomeUn() {
                 <div>광고 문의 : a1234567@email.com</div>
                 <div>고객 문의(email) : b1234567@email.com</div>
                 <div>고객 문의(call) : 000-0000-0000</div>
-                <div> -오전 9:30 ~ 17:30(주말,공휴일 제외)</div>
+                <div> -오전 9:30 ~ 오후 5:30(주말,공휴일 제외)</div>
             </footer>
         </div>
     );
